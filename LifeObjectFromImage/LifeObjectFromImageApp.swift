71ed6f250f39d455251277d1d186d6a155563429
MszPro <code_sign@mszpro.com>
2024-05-25 21:56:23 +0900

    Initial Commit

---
+++ LifeObjectFromImage/LifeObjectFromImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeObjectFromImageApp.swift
+//  LifeObjectFromImage
+//
+//  Created by Msz on 2024/05/25.
+//
+
+import SwiftUI
+
+@main
+struct LifeObjectFromImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
